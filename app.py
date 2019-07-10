e543381f43f8445a79bdf171075a5114eda9f4c8
Acejack7 <adasio7@gmail.com>
2019-07-10 22:50:33 +0200

    channel creation alpha

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = ['general']
+
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html", channels=channels)
+
+
+@socketio.on("new channel")
+def vote(data):
+    channel = data["channel_name"]
+    channels.append(channel)
+    emit("current channels", {'new_channel': channel}, broadcast=True)
+
+
+socketio.run(app, host="127.0.0.121")
---
+++ static/index.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // show all current channels
+    /*var channels = '{{ channels }}';
+    channels.array.forEach(element => {
+        const li = document.createElement('li');
+        li.innerHTML = `New channel: ${element}`;
+        document.querySelector('#channels').append(li);
+    });*/
+
+    // connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // user provides channel name
+    socket.on('connect', () => {
+        document.querySelector('#submit_channel').onclick = () => {
+            const channel_name = document.querySelector('#channel_name').value;
+            socket.emit('new channel', {'channel_name': channel_name});
+        };
+    });
+
+    // show new channel to all
+    socket.on('current channels', data => {
+        const channel = data.channel;
+        const li = document.createElement('li');
+        li.innerHTML = `New channel: ${channel}`;
+        document.querySelector('#channels').append(li);
+    });
+
+    // check if user already used the chat. If not - ask for name.
+    if (!localStorage.getItem('name')) {
+        document.querySelector('#message_to_user').innerHTML = "Please provide the name."
+    } else {
+        name = localStorage.getItem('name');
+        document.querySelector('#form_name').style.display = 'none';
+        document.querySelector('#message_to_user').innerHTML = `Welcome ${name}!`;
+    };
+
+    // user provides his/her name
+    document.querySelector('#submit_name').onclick = () => {
+        const username = document.querySelector('#username').value;
+        localStorage.setItem('name', username);
+    };
+
+});
