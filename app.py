26c1ee3b3e4bffe4e2ab62ef62e8ef3df645b251
Acejack7 <adasio7@gmail.com>
2019-07-11 17:54:18 +0200

    update 4 - non usable

--- app.py
+++ app.py
@@ -7,19 +7,33 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = ['general']
+channels = ['general', 'afk']
+
+messages = {'general': [], 'afk': []}
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", channels=channels)
+    return render_template("index.html", channels=channels, messages=messages)
 
 
 @socketio.on("new channel")
-def vote(data):
+def create_channel(data):
     channel = data["channel_name"]
     channels.append(channel)
+    messages[channel] = []
     emit("current channels", {'new_channel': channel}, broadcast=True)
 
 
-socketio.run(app, host="127.0.0.121")
+@socketio.on("new message")
+def new_message(data):
+    user = data["user"]
+    message = data["message"]
+    channel = data["channel"]
+
+    messages[channel].append((user, message))
+    message_details = {'user': user, 'message': message, 'channel': channel}
+    emit("message added", message_details, broadcast=True)
+
+
+socketio.run(app, host="0.0.0.0")
--- static/index.js
+++ static/index.js
@@ -1,24 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-    // user provides channel name
-    socket.on('connect', () => {
-        document.querySelector('#submit_channel').onclick = () => {
-            const channel_name = document.querySelector('#channel_name').value;
-            socket.emit('new channel', {'channel_name': channel_name});
-        };
-    });
-
-    // show new channel to all
-    socket.on('current channels', data => {
-        const channel = data.channel;
-        const li = document.createElement('li');
-        li.innerHTML = `New channel: ${channel}`;
-        document.querySelector('#channels').append(li);
-    });
-
     // check if user already used the chat. If not - ask for name.
     if (!localStorage.getItem('name')) {
         document.querySelector('#message_to_user').innerHTML = "Please provide the name."
@@ -28,10 +9,82 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#message_to_user').innerHTML = `Welcome ${name}!`;
     };
 
+
+
     // user provides his/her name
     document.querySelector('#submit_name').onclick = () => {
         const username = document.querySelector('#username').value;
         localStorage.setItem('name', username);
     };
 
+    // user clicks a channel
+    document.querySelectorAll('.btn-channel').forEach(button => {
+        button.onclick = () => {
+            const clicked_channel = button.dataset.name;
+            localStorage.setItem('current_channel', clicked_channel);
+        };
+    });
+
+    // connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    
+    socket.on('connect', () => {
+        // user provides channel name
+        document.querySelector('#submit_channel').onclick = () => {
+            const channel_name = document.querySelector('#channel_name').value;
+            socket.emit('new channel', {'channel_name': channel_name});
+        };
+
+        // user sends new message
+        document.querySelector('#submit_message').onclick = () => {
+            const message = document.querySelector('#message').value;
+            const message_details = {
+                'message': message,
+                'user': localStorage.getItem('name'),
+                'channel': localStorage.getItem('current_channel')
+            };
+            socket.emit('new message', message_details);
+        };
+    });
+
+    // show new channel to all
+    socket.on('current channels', data => {
+        const channel = data.new_channel;
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.setAttribute('class', 'btn btn-primary btn-channel');
+        button.setAttribute('name', 'channel');
+        button.innerHTML = `${channel}`;
+        li.appendChild(button);
+        document.querySelector('#channels').append(li);
+    });
+
+    // show new message to all
+    socket.on('message added', data => {
+        const user = data.user;
+        const message = data.message;
+        const li = document.createElement('li');
+        li.innerHTML = `${user}: ${message}`;
+        document.querySelector('#messages').append(li);
+    });
+
+    var messages_to_show = "{{ messages|safe }}";
+    console.log(messages_to_show);
+
+    // check if current channel exists; if not - pick general
+    if (!localStorage.getItem('current_channel')) {
+        localStorage.setItem('current_channel', 'general');
+        current_channel = localStorage.getItem('current_channel');
+    };
+    
+    // set the color for current channel
+    current_channel = localStorage.getItem('current_channel');
+    document.querySelectorAll('.btn-channel').forEach(button => {
+        const button_channel = button.dataset.name;
+        if (button_channel == current_channel) {
+            button.style.color = "red";
+            console.log(button);
+        };
+    });
 });
