334ebd0d411a21409a23ac76d538f0938f8880b2
Acejack7 <adasio7@gmail.com>
2019-07-13 00:48:10 +0200

    Almost all functionalities of Project 2

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ import os
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = ['general', 'afk']
+channels = ["general", "afk"]
 
-messages = {'general': [], 'afk': []}
+messages = {'general': [("2019-07-12 23:39:07", "test", "hey!"),
+                        ("2019-07-12 23:39:07", "test", "how are you?")],
+            'afk': [("2019-07-12 23:39:07", "joe", "I like cookies!"),
+                    ("2019-07-12 23:39:07", "dummy", "Who cares?")]}
+
+last_channels = {}
 
 
 @app.route("/")
@@ -17,12 +23,19 @@ def index():
     return render_template("index.html", channels=channels, messages=messages)
 
 
+@socketio.on("save current channel")
+def save_channel(data):
+    user = data["username"]
+    channel = data["channel"]
+    last_channels[user] = channel
+
+
 @socketio.on("new channel")
 def create_channel(data):
     channel = data["channel_name"]
     channels.append(channel)
     messages[channel] = []
-    emit("current channels", {'new_channel': channel}, broadcast=True)
+    emit("current channels", {"new_channel": channel}, broadcast=True)
 
 
 @socketio.on("new message")
@@ -31,8 +44,12 @@ def new_message(data):
     message = data["message"]
     channel = data["channel"]
 
-    messages[channel].append((user, message))
-    message_details = {'user': user, 'message': message, 'channel': channel}
+    now = datetime.now()
+    formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    messages[channel].append((formatted_now, user, message))
+    message_details = {"user": user, "message": message,
+                       "channel": channel, "timestamp": formatted_now}
     emit("message added", message_details, broadcast=True)
 
 
--- static/index.js
+++ static/index.js
@@ -6,29 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         name = localStorage.getItem('name');
         document.querySelector('#form_name').style.display = 'none';
-        document.querySelector('#message_to_user').innerHTML = `Welcome ${name}!`;
+        document.querySelector('#message_to_user').innerHTML = `Your name: ${name}`;
     };
 
-
-
-    // user provides his/her name
+    // user provides the name
     document.querySelector('#submit_name').onclick = () => {
         const username = document.querySelector('#username').value;
         localStorage.setItem('name', username);
     };
 
-    // user clicks a channel
-    document.querySelectorAll('.btn-channel').forEach(button => {
-        button.onclick = () => {
-            const clicked_channel = button.dataset.name;
-            localStorage.setItem('current_channel', clicked_channel);
-        };
-    });
-
     // connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    
     socket.on('connect', () => {
         // user provides channel name
         document.querySelector('#submit_channel').onclick = () => {
@@ -46,6 +35,20 @@ document.addEventListener('DOMContentLoaded', () => {
             };
             socket.emit('new message', message_details);
         };
+
+        // user clicks a channel
+        document.querySelectorAll('.btn-channel').forEach(button => {
+            button.onclick = () => {
+                const clicked_channel = button.dataset.name;
+                localStorage.setItem('current_channel', clicked_channel);
+                const username = localStorage.getItem('name');
+                current_channel_details = {
+                    'username': username,
+                    'channel': clicked_channel
+                }
+                socket.emit('save current channel', current_channel_details);
+            };
+        });
     });
 
     // show new channel to all
@@ -62,15 +65,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // show new message to all
     socket.on('message added', data => {
+        const timestamp = data.timestamp;
         const user = data.user;
         const message = data.message;
         const li = document.createElement('li');
-        li.innerHTML = `${user}: ${message}`;
+        li.innerHTML = `${timestamp} | ${user}: ${message}`;
         document.querySelector('#messages').append(li);
     });
-
-    var messages_to_show = "{{ messages|safe }}";
-    console.log(messages_to_show);
 
     // check if current channel exists; if not - pick general
     if (!localStorage.getItem('current_channel')) {
